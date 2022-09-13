9e9661f753541d056a2008b4b3218c88e98ec96a
extra-kool <impactworld96@gmail.com>
2022-09-12 20:41:49 -0700

    agbeg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
-* main - Printing
+* main - Print the string in the put function 
+* Description: using the main function 
 *
-* Return: Always 0 (Successful)
+* Return: 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual 
+      puts("\"Programming is like building a multilingual 
 puzzle");
-return (0); 
+return (0);
 }

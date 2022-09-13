316233dc32e72fb2e7631aef4e27e162289480f3
extra-kool <impactworld96@gmail.com>
2022-09-12 20:31:06 -0700

    do hard things

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
+* main - Printing
 *
-* Description using the main function
-* this program prints 'programming is like building a multilingual puzzle'
-* Return: 0
+* Return: Always 0 (Successful)
 */
 int main(void)
 {
-        puts(*\*Programming is like building a building a multilingual puzzle*);
-        Return (0);
+puts("\"Programming is like building a multilingual 
+puzzle");
+return (0); 
 }

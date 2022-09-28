d43f4b9d20a597715f6f7b2f1d950c9da9b2d3e4
extra-kool <impactworld96@gmail.com>
2022-09-28 06:25:33 -0700

    protoptype 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- * @dest: string that will be appended
- * @src: string to be concatenated upon
+ * _memset - fill memory with a constant byte
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: bytes of memory area to fill
  *
- * Return: return poiner to @dest
+ * Return: the memory area filled
  */
-
-char *_strcat(char *dest, char *src)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]:index++)
-		dest [dest_len++] = src[index];
-	return (dest);
+	unsigned int a;
+	for (a = 0; a<n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
 }
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypess()
+ * int prototypes()
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_ strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif /*MAIN_H*/

d5fc3f0d936e203672343a6ad82f3acfaac2b3aa
extra-kool <impactworld96@gmail.com>
2022-10-02 22:45:39 -0700

    wow

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,16 +2,16 @@
 #define MAIN_H
 
 /**
- * void prototypes()
- * int prototypes()
- */
+* void prototypes()
+* int prototypes()
+*/
 
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
-char *_ strchr(char *s, char c);
+char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strbrk(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
